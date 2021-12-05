c54fc215c0f0d7e4f4c2cf0dbdbf72f55a72e4ef
Gaby Dias <36906363+gabydias@users.noreply.github.com>
2021-12-04 21:55:49 -0300

    3

--- app.py
+++ app.py
@@ -1,38 +1,15 @@
-#main.py
-from flask import Flask, jsonify
+import os
+from flask import Flask, json, jsonify
+
 app = Flask(__name__)
-infos = [
-    {
-        "integrante": 1,
-        "estado": "SP",
-        "idade" : 26,
-        "pet" : "true",
-        "music": [
-            {"name":"Chamego", "estilo": "MBP"}
-        ]
-    },
-    {
-        "integrante": 2,
-        "estado": "RJ",
-        "idade" : 40,
-        "pet" : "false",
-        "music": [
-            {"name":"Coisa Linda", "estilo": "MBP"}
-        ]
-    },
-    {
-        "integrante": 4,
-        "estado": "MG",
-        "idade" : 52,
-        "pet" : "true",
-        "music": [
-            {"name":"Aquarela", "estilo": "MBP"}
-        ]
-    }
-]
+
 @app.route('/')
 def home():
-    return jsonify(infos)
+    filename = os.path.join(app.static_folder, 'infos.json')
+    with open(filename) as test_file:
+      data = json.load(test_file)
+
+    return jsonify(data)
 
 if __name__ == '__main__':
   app.run()
